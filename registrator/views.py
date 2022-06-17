55d463039e392f84fda0f32d718b66773d61feb4
Kirill Murzin <murz.kirill@yandex.ru>
2022-06-17 12:16:03 +0500

    registrator key generation

--- registrator/views.py
+++ registrator/views.py
@@ -1,6 +1,24 @@
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+import rsa
+
+public_key = None
+private_key = None
 
 
 def index(request):
-    return HttpResponse(render(request, 'registrator.html'))
+    voters = User.objects.all()
+    return HttpResponse(render(request, 'registrator.html', {'voters': voters,
+                                                             'public_key': None if public_key is None else f'e={public_key.e}\nn={public_key.n}',
+                                                             'private_key': None if private_key is None else f'e={private_key.e}\nn={private_key.n}\n'
+                                                                                                             f'p={private_key.p}\nq={private_key.q}\nd={private_key.d}'}))
+
+
+def generate_keys(request):
+    global public_key, private_key
+    if request.method == 'POST':
+        public_key, private_key = rsa.newkeys(128)
+        return JsonResponse({'public_key': f'e={public_key.e}\nn={public_key.n}',
+                             'private_key': f'e={private_key.e}\nn={private_key.n}\n'
+                                            f'p={private_key.p}\nq={private_key.q}\nd={private_key.d}'})
--- static/scripts/registrator.js
+++ static/scripts/registrator.js
@@ -1,3 +1,28 @@
-window.onload = function() {
+import {getCookie} from "./utilities.js";
+
+let csrftoken;
+window.onload = function () {
     $("#nav-registrator").addClass('active');
-  };
+    csrftoken = getCookie('csrftoken');
+
+};
+
+$('#generate-keys-button').on('click', () => {
+    $.ajax({
+        url: 'generate_keys',
+        type: 'post',
+        // data: {
+        //     access_token: 'XXXXXXXXXXXXXXXXXXX'
+        // },
+        headers: {
+            'X-CSRFToken': csrftoken,
+        },
+        // dataType: 'json',
+        success: function (data) {
+            console.log(data);
+            $('#keys-div').removeAttr('hidden');
+            $('#public-key').text(data.public_key);
+            $('#private-key').text(data.private_key);
+        }
+    });
+})
